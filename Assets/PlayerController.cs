04abf3629c59ea4f0371788375aae492c2c6bf55
Neroun <ALekseychaykin2005@gmail.com>
2022-03-30 00:05:58 +0300

    Добавил несколько элементов сцены и недоделанный скрипт передвижения

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+    private float MoveDirection;
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 3f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDirection = Input.GetAxis("Horizontal"); 
+        
+    }
+}
