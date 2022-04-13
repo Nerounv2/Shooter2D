49d1cc9e98026e778b3377adf4c70aaae42fe254
Neroun <ALekseychaykin2005@gmail.com>
2022-04-13 22:52:36 +0300

    BulletScript doesn't work(

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript: MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed = 20f;
+
+    private Rigidbody2D bulletRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbody.velocity = transform.right * bulletSpeed; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
